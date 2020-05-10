585a50e5c3dec4db6c27c40dc12dd22398dea958
Nate Bierdeman <natebierdeman@gmail.com>
2020-05-10 15:52:58 -0500

    Created Login component

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../views/Home.vue";
 import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
@@ -20,6 +21,7 @@ const routes = [
     component: () => import(/* webpackChunkName: "about" */ "../views/About.vue"),
   },
   { path: "/signup", name: "signup", component: Signup },
+  { path: "/login", name: "login", component: Login },
 ];
 
 const router = new VueRouter({
