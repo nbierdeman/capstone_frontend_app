5bbaa301436c2b80eed6efe73aa9ee283943a08b
Nate Bierdeman <natebierdeman@gmail.com>
2020-05-15 11:09:08 -0500

    Created styling, added images, and wrote copy for the about page.

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
-    <h3>Bikr is a cycling app for the city of Chicago that provides air quality information for your ride.</h3>
+    <!-- Masthead2-->
+    <header class="masthead2">
+      <div class="container d-flex h-100 align-items-center">
+        <div class="col-lg-8 mx-auto">
+          <h2 class="text-white mb-4">Built with Array of Things</h2>
+            <p class="text-white-50">
+              Bikr is a Chicago cycling app that provides air quality information for your ride. It uses  
+              <a href="https://arrayofthings.github.io/" target="_blank">Array of Things</a>
+              to keep you up to date on the air you breathe.
+            </p>
+        </div>
+      </div>
+    </header>
   </div>
 </template>
