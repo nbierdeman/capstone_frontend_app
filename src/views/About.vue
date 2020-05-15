90e7e05210809d0d34d300bf80eecf35e2031cc7
Nate Bierdeman <natebierdeman@gmail.com>
2020-05-15 11:28:01 -0500

    Fixed indentation

--- src/views/About.vue
+++ src/views/About.vue
@@ -5,11 +5,11 @@
       <div class="container d-flex h-100 align-items-center">
         <div class="col-lg-8 mx-auto">
           <h2 class="text-white mb-4">Built with Array of Things</h2>
-            <p class="text-white-50">
-              Bikr is a Chicago cycling app that provides air quality information for your ride. It uses  
-              <a href="https://arrayofthings.github.io/" target="_blank">Array of Things</a>
-              to keep you up to date on the air you breathe.
-            </p>
+          <p class="text-white-50">
+            Bikr is a Chicago cycling app that provides air quality information for your ride. It uses  
+            <a href="https://arrayofthings.github.io/" target="_blank">Array of Things</a>
+            to keep you up to date on the air you breathe.
+          </p>
         </div>
       </div>
     </header>
